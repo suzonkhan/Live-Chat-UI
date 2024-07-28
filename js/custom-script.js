6cf5dc9ac33ba45a17e6e04ae6f57f0dff4a0b74
Abdur Rahman Suzon <arsuzon.khan@gmail.com>
2024-07-28 15:07:45 +0600

    Initial Relese

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,10 +1,9 @@
 jQuery (document).ready(function() {
     // Function to check if element is in viewport
-
-    jQuery(".floating-contact-button").on('click', function () {
-        $(".floating-contact-wrapper").toggleClass("active-floating-contact")
+    jQuery(".floating-chat-button").on('click', function () {
+        $(".floating-chat-wrapper").toggleClass("active-floating-contact")
     })
     jQuery(".close-floating-contact").on('click', function () {
-        $(".floating-contact-wrapper").removeClass("active-floating-contact")
+        $(".floating-chat-wrapper").removeClass("active-floating-contact")
     })
 });
